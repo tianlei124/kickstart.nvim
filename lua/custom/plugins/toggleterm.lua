135b0d68db969d2bfb1d6536a1be6e1539c88682
tianlei.richard <tianlei.richard@bytedance.com>
2025-01-22 14:40:40 +0800

    enable clangd and rust-analyser, add toogleterm and barbaric.

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,24 @@
+return {
+  'akinsho/toggleterm.nvim',
+  config = function()
+    require('toggleterm').setup {
+      size = 20,
+      open_mapping = [[<c-\>]],
+      direction = 'float',
+      shell = 'zsh',
+      float_opts = {
+        border = 'curved',
+      },
+    }
+
+    function _G.set_terminal_keymaps()
+      local opts = { buffer = 0 }
+      vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+    end
+
+    -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+    vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
+  end,
+  keys = [[<c-\>]],
+  cmd = 'ToggleTerm',
+}
